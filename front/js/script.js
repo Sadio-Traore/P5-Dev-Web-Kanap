ae779d837207ad3ddb9ff7c9e7cb545677173394
Sadio Traoré <sadiotraore1@gmail.com>
2022-04-12 17:44:02 +0200

    Ajout des produits à la page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,64 @@
+// récupérer les donées du serveur
+const url = 'http://localhost:3000/api/products'
+
+// Récuperation des produits
+async function recupererProduit() {
+const requete = await fetch(url, {
+  method:'GET',
+})
+
+if(!requete.ok){
+  alert('une erreur est survenue');
+}
+else {
+  var items = document.getElementsByClassName('.items');
+   items = await requete.json();
+ // console.log(donnees);
+ // Iteration sur chaque données
+ for (const product of items){
+   console.log (product);
+
+   // création lien vers chaque produit
+  productLink = document.createElement('a');
+  document.querySelector('#items').append(productLink);
+  productLink.href = './product.html?id=${items[product]._id}';
+
+  // cration d'un bloc article pour chaque produit
+  const article = document.createElement ('article');
+  productLink.append(article);
+  
+  // création img et attriubut src alt de chaque produit
+  const img = document.createElement ('img');
+  article.append(img);
+  img.src   =product.imageUrl;
+  img.alt   = product.alTxt;
+  
+  // Nom de chaque Produit
+  const h3  = document.createElement ('h3');
+  article.append(h3);
+  h3.classList.add("productName");
+  h3.textContent = product.name;
+  
+  // Description produit
+  const p = document.createElement ('p');
+  article.append(p);
+  p.classList.add("productDescription");
+  p.textContent = product.description;
+
+ }
+}
+}
+recupererProduit();
+
+
+  
+ 
+
+
+
+
+  
+
+
+
+
