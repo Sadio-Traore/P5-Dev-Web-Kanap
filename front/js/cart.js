8670b7370dce05c261be13d3bfe091b14ea6faa8
Sadio Traoré <sadiotraore1@gmail.com>
2022-04-19 19:28:40 +0200

    récupération des produits ajoutés au panier

---
+++ front/js/cart.js
@@ -0,0 +1,6 @@
+
+//Récupération des produits ajoutés au panier contenus dans le localStorage
+let localStorageProduct = JSON.parse(localStorage.getItem('product'));
+console.log(localStorageProduct);
+
+
