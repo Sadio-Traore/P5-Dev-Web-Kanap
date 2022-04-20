af6a31078806df6610db56adfdaabd5e760a6cd1
Sadio Traoré <sadiotraore1@gmail.com>
2022-04-20 12:26:29 +0200

    suite création des balises HTML en JS

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,11 +7,13 @@ let article = document.createElement('article');
 article.classList.add("cart__item");
 document.querySelector("#cart__items").append(article);
 
+
 let cartItemImg = document.createElement('div');
 cartItemImg.classList.add("cart__item__img");
 article.append(cartItemImg);
 
 let itemImg  = document.createElement('img');
+//itemImg.tagName.add('img');
 //itemImg.src = product.imageUrl;
 //itemImg.alt = product.altTxt;
 cartItemImg.append(itemImg);
@@ -22,17 +24,38 @@ itemContent.classList.add("cart__item__content");
 article.append(itemContent);
 
 let itemDescription = document.createElement('div');
-itemDescription.classList.add("cart__item__content__description");
+itemDescription.classList.add("cart__item__content__titlePrice");
 itemContent.append(itemDescription);
 
  let itemName  = document.createElement('h2');
- let itemColor  = document.createElement('p')
- let itemPrice = document.createElement('p')
+ itemDescription.append(itemName);
 
-let itemSettings = document.createElement('div')
-let itemSettingsQuantity  = document.createElement('div')
-let itemSettingsP  = document.createElement('p')
-let itemQuantity  = document.createElement('input')
+ let itemColor  = document.createElement('p');
+ itemDescription.append(itemColor);
 
-let itemSettingsDelete = document.createElement('div')
-let deleteItem = document.createElement('p')
+ let itemPrice = document.createElement('p');
+ itemDescription.append(itemPrice);
+
+
+let itemSettings = document.createElement('div');
+itemSettings.classList.add("cart__item__content__settings");
+article.append(itemSettings);
+
+let itemSettingsQuantity  = document.createElement('div');
+itemSettingsQuantity.classList.add("cart__item__content__settings__quantity");
+itemSettings.append(itemSettingsQuantity);
+
+let itemSettingsP  = document.createElement('p');
+itemSettingsQuantity.append(itemSettingsP);
+
+let itemQuantity  = document.createElement('input');
+itemSettingsQuantity.append(itemQuantity);
+
+
+let itemSettingsDelete = document.createElement('div');
+itemSettingsDelete.classList.add("cart__item__content__settings__delete");
+article.append(itemSettingsDelete);
+
+let deleteItem = document.createElement('p');
+itemSettingsDelete.append(deleteItem);
+
